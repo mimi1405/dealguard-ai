73d711fb71e23dcd4bb36d69cad6383bc5f1c3df
Maksim Momcilovic <69592594+mimi1405@users.noreply.github.com>
2026-02-07 20:46:59 +0100

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,6 @@ export default function Home() {
           <CodexTrailBackground />
               <HoverVideoIcon
                 src="https://fcwdgsfspejqfyjmazvk.supabase.co/storage/v1/object/public/web_data/final.mp4" poster="/brain-poster.png"/>
-        </div>
               <h1 className="text-8xl font-bold mb-6 bg-gradient-to-r from-white via-white/60 to-white bg-clip-text text-transparent">
                 scoraxAI
               </h1>
