640639f3a063fccb4ce8bbcc8cdb3edda4d62286
Maksim Momcilovic <69592594+mimi1405@users.noreply.github.com>
2026-02-08 03:19:28 +0100

    Updated brain-animation.tsx

--- components/analysis/brain-animation.tsx
+++ components/analysis/brain-animation.tsx
@@ -124,5 +124,7 @@ export function BrainAnimation({
     );
   }
 
-  return ( <div ref={containerRef} className={w-full h-full bg-[#0b0d10] ${className ?? ''}} /> );
+  return (
+    <div ref={containerRef} className={w-full h-full bg-[#0b0d10] ${className ?? ''}} /><div ref={containerRef} className={w-full h-full bg-[#0b0d10] ${className ?? ''}} />
+  );
 }
